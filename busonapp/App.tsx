013ceba77ed482ef4c91e6c533f9739b22776a76
alfredlucero <alfred.lucero@ucla.edu>
2019-12-30 23:29:24 -0800

    Added button with flex styles and installed react navigation dependencies

--- busonapp/App.tsx
+++ busonapp/App.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Buson</Text>
-      <Text style={styles.subHeader}>Secure box for your mail drops!</Text>
+    <View style={styles.landingContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Buson</Text>
+        <Text style={styles.subHeader}>A secure box for your mail drops!</Text>
+      </View>
+
+      <View style={styles.authContainer}>
+        <View style={styles.signupButton}>
+          <Button onPress={() => {}} title="Sign Up" color="steelblue" />
+        </View>
+        <View style={styles.loginButton}>
+          <Button onPress={() => {}} title="Login" color="#fcfcfc" />
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  landingContainer: {
+    flex: 1,
+  },
+  headerContainer: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
   header: {
-    color: "#595959",
+    color: "steelblue",
     fontWeight: "bold",
     fontSize: 35,
     marginBottom: 10,
@@ -27,4 +41,30 @@ const styles = StyleSheet.create({
     color: "#595959",
     fontSize: 20,
   },
+  authContainer: {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis: 90,
+    width: "100%",
+    flexDirection: "row",
+  },
+  signupButton: {
+    width: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "steelblue",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  loginButton: {
+    width: "50%",
+    backgroundColor: "steelblue",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "transparent",
+    borderRadius: 5,
+  },
 });
+
+export default App;
